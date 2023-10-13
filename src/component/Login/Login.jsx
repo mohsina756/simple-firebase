a9904258ff40b85ecc85643565764eaa32ce37ed
mati <matee803@gmail.com>
2023-10-13 23:10:27 +0600

    last commit added

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,26 +1,76 @@
-
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
-import app from '../../firebase/firebase.init';
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../../firebase/firebase.init";
+import { useState } from "react";
 const Login = () => {
-    const auth = getAuth(app);
-    console.log(app);
-    const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
+  const auth = getAuth(app);
+  console.log(app);
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
-    const handleGoogleSignIn = () =>
+  const handleGoogleSignIn = () =>
     // console.log('google is coming')
-    signInWithPopup(auth, provider)
-    .then(result => {
-        const user = result.user;
-        console.log(user);
-    })
-    .catch(error => {
-        console.log('error', error.message)
-    })
-    return (
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+      const handleGithubSignIn = () =>{
+        signInWithPopup(auth, githubProvider)
+        .then( result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            setUser(loggedUser);
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+      }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      {/* user ? logout : sign in  */}
+
+      {user ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSignIn}>Google login</button>
+          <button onClick={handleGithubSignIn}>Github Login</button>
+        </>
+      )}
+
+      {user && (
         <div>
-            <button onClick={handleGoogleSignIn}>Google login</button>
+          <h3>User: {user.displayName}</h3>
+          <p>Email: {user.email}</p>
+          {/* <img src={user.photoURL} alt="" /> */}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Login;
